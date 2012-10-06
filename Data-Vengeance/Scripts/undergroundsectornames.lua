c9d86ceb916b9d1454f20c88cc305ff2d9c56299
DepressivesBrot <>
2012-10-06 14:49:12 +0000

    Initial Version on the basis of Vengeance-1.13_Reloaded_English_SCI_non-public_alpha_05.10.2012 - removed most of the files that were either redundant within this data set or to the official 4870 release ----------------------------------------------------------------------------------------------------------- Changes: - included exes with latest version string change - swapped AIM and Maps in the vfs hierarchy so AIM's smp#items and smguns.sti takes precedence

---
+++ Data-Vengeance/Scripts/undergroundsectornames.lua
@@ -0,0 +1,66 @@
+﻿--[[
+
+    This file (or its localized versions respectively) are included in
+    "initunderground.lua" to provide the underground sector names.
+    
+    Make sure to save this file as UTF-8, preferably with BOM signature in
+    order to help editors to not mess up the encoding. Also make sure to not
+    save BOM in any other script unless it explicitly allows that.
+
+]]
+
+sectornames = {
+
+    ["A10-1"] = "Hideout",
+    
+    ["J9-1"] = "Tixa Dungeon",
+    ["J9-2"] = "Tixa Dungeon",
+    
+    ["K4-1"] = "Orta Basement",
+    
+    ["O3-1"] = "Tunnel",
+    
+    ["P3-1"] = "Shelter",
+    
+    
+    ["B2-1"] = "Oronegro Mine",
+    
+    ["D4-1"] = "San Mona Mine",
+    ["D5-1"] = "San Mona Mine",
+    
+    ["D13-1"] = "Drassen Mine",
+    ["E13-1"] = "Drassen Mine",
+    ["E13-2"] = "Drassen Mine",
+    ["F13-2"] = "Drassen Mine",
+    ["G13-2"] = "Drassen Mine",
+    ["G13-3"] = "Drassen Mine",
+    ["F13-3"] = "Drassen Mine",
+    
+    ["H8-1"] = "Salinas Mine",
+    ["H9-1"] = "Salinas Mine",
+    ["H9-2"] = "Salinas Mine",
+    ["H8-2"] = "Salinas Mine",
+    ["H8-3"] = "Salinas Mine",
+    ["I8-3"] = "Salinas Mine",
+    ["J8-3"] = "Salinas Mine",
+
+    ["I14-1"] = "Mela Mine",
+    ["J14-1"] = "Mela Mine",
+    ["J14-2"] = "Mela Mine",
+    ["J13-2"] = "Mela Mine",
+    ["J13-3"] = "Mela Mine",
+    ["K13-3"] = "Mela Mine",
+    
+    ["H3-1"] = "Doran Mine",
+    ["I3-1"] = "Doran Mine",
+    ["I3-2"] = "Doran Mine",
+    ["H3-2"] = "Doran Mine",
+    ["H4-2"] = "Doran Mine",
+    ["H4-3"] = "Doran Mine",
+    ["G4-3"] = "Doran Mine",
+    
+    ["P1-1"] = "Demoville",
+    ["P1-2"] = "Demoville",
+ 
+    creatureLair = "Creature Lair",
+}
