2b91b452c4eaf50e6c1714081d6a9bc7b4350da6
Andrzej Fałkowski <vkowsky@gmail.com>
2014-03-09 18:45:53 +0000

    - changed Mendax's approach modifiers - changed Mendax's hair color to white - replaced Eddie's portrait in terrorist files with Morris's - changed "Grand" -> "Grandee" - added batch script "ProfCloner.bat" that copies Prof.dat to all Prof_Difficulty_AmountOfGuns.dat, since apparently values in them overwrite not only starting equipment in OIS, but ALL profile data as well and I don't feel like modifying 9 identical files for every single change

---
+++ Data-Vengeance/BinaryData/ProfCloner.bat
@@ -0,0 +1,8 @@
+COPY Prof.dat Prof_Experienced_NormalGuns.dat
+COPY Prof.dat Prof_Experienced_TonsOfGuns.dat
+COPY Prof.dat Prof_Expert_NormalGuns.dat
+COPY Prof.dat Prof_Expert_TonsOfGuns.dat
+COPY Prof.dat Prof_Insane_NormalGuns.dat
+COPY Prof.dat Prof_Insane_TonsOfGuns.dat
+COPY Prof.dat Prof_Novice_NormalGuns.dat
+COPY Prof.dat Prof_Novice_TonsOfGuns.dat
